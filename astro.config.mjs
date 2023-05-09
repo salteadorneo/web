d3ec25fd0de5764387c387e48f17da6d6c6f0026
Cristian Adán <cristian@doersdf.com>
2023-05-09 15:06:39 +0200

    remove sitemap astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
-import sitemap from "@astrojs/sitemap";
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://salteadorneo.dev',
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind()]
 });
