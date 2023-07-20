cb51effeca234dba97662abbb8e29955db871874
Cristian Adán <cristian@doersdf.com>
2023-07-20 17:47:15 +0200

    add sitemap + update deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://salteadorneo.dev',
-  integrations: [tailwind()]
+  integrations: [tailwind(), sitemap()]
 });
