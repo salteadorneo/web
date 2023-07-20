a07811a16a2d8f1a02fcd9d4466b624f66f780e5
Cristian Adán <hola@salteadorneo.dev>
2023-07-20 20:12:00 +0200

    add astro alias

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://salteadorneo.dev',
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), sitemap()],
 });
