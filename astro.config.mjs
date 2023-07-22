1f02acb0aa9df7cc483d0f21316f1403f1da4e2c
Cristian Adán <salteadorneo@users.noreply.github.com>
2023-07-22 12:17:14 +0200

    1.1.0 (#7)
    * add sitemap + update deps
    
    * add page tag WIP
    
    * add link tags
    
    * add astro alias
    
    * use alias
    
    * refactor + new structure view transitions
    
    * 1.1.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://salteadorneo.dev',
-  integrations: [tailwind()]
+  integrations: [tailwind(), sitemap()],
 });
