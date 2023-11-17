7ecb5f8d4d8860c2a68c32ba856b2dd803f5c9b2
Cristian Adán <hola@salteadorneo.dev>
2023-11-17 19:38:55 +0100

    remove contact form

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,21 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			animation: {
+				fadeInTop: "fadeInTop 1s ease-out forwards",
+			},
+			keyframes: {
+				fadeInTop: {
+					"0%": {
+						opacity: 0,
+						transform: "translateY(40px)",
+					},
+					"100%": {
+						opacity: 1,
+						transform: "translateY(0)",
+					},
+				},
+			},
 			colors: {
 				primary: "var(--color-primary)",
 				text: "var(--color-text)",
