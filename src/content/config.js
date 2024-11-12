2fa8dd796fc82d02a7281b066cd5bc8708f14e1c
Cristian Adán <hola@salteadorneo.dev>
2024-11-12 01:19:30 +0100

    feat: Enhance blog collection schema with validation using Zod

--- src/content/config.js
+++ src/content/config.js
@@ -1,9 +1,16 @@
-// 1. Import utilities from `astro:content`
-import { defineCollection } from 'astro:content';
-// 2. Define your collection(s)
-const blogCollection = defineCollection({ /* ... */ });
-// 3. Export a single `collections` object to register your collection(s)
-//    This key should match your collection directory name in "src/content"
+import { z, defineCollection } from 'astro:content';
+
+const blogCollection = defineCollection({ 
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    publishedDate: z.date(),
+    tags: z.array(z.string()),
+    image: z.string().optional(),
+  })
+});
+
 export const collections = {
   'blog': blogCollection,
 };
